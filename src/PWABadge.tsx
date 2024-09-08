328d008526ed84f0ab0d7df04b1afacca6c583f6
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2024-09-08 20:19:42 +0200

    prettier

--- src/PWABadge.tsx
+++ src/PWABadge.tsx
@@ -1,13 +1,12 @@
 import './PWABadge.css'
 
-import { useRegisterSW } from 'virtual:pwa-register/react'
+import {useRegisterSW} from 'virtual:pwa-register/react'
 
 function PWABadge() {
   // check for updates every hour
   const period = 60 * 60 * 1000
 
   const {
-    
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
   } = useRegisterSW({
@@ -15,35 +14,33 @@ function PWABadge() {
       if (period <= 0) return
       if (r?.active?.state === 'activated') {
         registerPeriodicSync(period, swUrl, r)
-      }
-      else if (r?.installing) {
+      } else if (r?.installing) {
         r.installing.addEventListener('statechange', (e) => {
           const sw = e.target as ServiceWorker
-          if (sw.state === 'activated')
-            registerPeriodicSync(period, swUrl, r)
+          if (sw.state === 'activated') registerPeriodicSync(period, swUrl, r)
         })
       }
     },
   })
 
   function close() {
-    
     setNeedRefresh(false)
   }
 
   return (
-    <div className="PWABadge" role="alert" aria-labelledby="toast-message">
-      { (needRefresh)
-      && (
-        <div className="PWABadge-toast">
-          <div className="PWABadge-message">
-            <span id="toast-message">New content available, click on reload button to update.</span>
-              
-              
+    <div className='PWABadge' role='alert' aria-labelledby='toast-message'>
+      {needRefresh && (
+        <div className='PWABadge-toast'>
+          <div className='PWABadge-message'>
+            <span id='toast-message'>New content available, click on reload button to update.</span>
           </div>
-          <div className="PWABadge-buttons">
-            <button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button>
-            <button className="PWABadge-toast-button" onClick={() => close()}>Close</button>
+          <div className='PWABadge-buttons'>
+            <button className='PWABadge-toast-button' onClick={() => updateServiceWorker(true)}>
+              Reload
+            </button>
+            <button className='PWABadge-toast-button' onClick={() => close()}>
+              Close
+            </button>
           </div>
         </div>
       )}
@@ -60,18 +57,16 @@ function registerPeriodicSync(period: number, swUrl: string, r: ServiceWorkerReg
   if (period <= 0) return
 
   setInterval(async () => {
-    if ('onLine' in navigator && !navigator.onLine)
-      return
+    if ('onLine' in navigator && !navigator.onLine) return
 
     const resp = await fetch(swUrl, {
       cache: 'no-store',
       headers: {
-        'cache': 'no-store',
+        cache: 'no-store',
         'cache-control': 'no-cache',
       },
     })
 
-    if (resp?.status === 200)
-      await r.update()
+    if (resp?.status === 200) await r.update()
   }, period)
 }
--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,7 @@
 /// <reference lib="webworker" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import {cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute} from 'workbox-precaching'
+import {clientsClaim} from 'workbox-core'
+import {NavigationRoute, registerRoute} from 'workbox-routing'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -13,14 +13,10 @@ cleanupOutdatedCaches()
 
 let allowlist: RegExp[] | undefined
 // in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+if (import.meta.env.DEV) allowlist = [/^\/$/]
 
 // to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html'), {allowlist}))
 
 self.skipWaiting()
 clientsClaim()
