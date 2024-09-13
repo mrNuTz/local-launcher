60ad6786f87d6ff642efb073d45b5707cae30293
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2024-09-13 19:51:47 +0200

    mantine setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@mantine/core/styles.css'
+import {MantineProvider} from '@mantine/core'
+
 import App from './App.tsx'
+import {theme} from './theme.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
