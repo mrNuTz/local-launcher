5616eea36491994eed482aa1c6f1a648f2b51e9f
Raphael Nussbaumer <nussbaumer@doerler.co.at>
2024-12-01 14:28:22 +0100

    rate limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,13 @@
 import {createServer} from 'express-zod-api'
 import {config} from './config'
 import {routing} from './routing'
+import {rateLimit} from 'express-rate-limit'
 
-createServer(config, routing)
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 10,
+})
+
+createServer(config, routing).then((server) => {
+  server.app.use(limiter)
+})
